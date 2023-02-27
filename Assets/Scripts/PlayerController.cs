71c87c651769b233dbf766b5c10519d8d250e0c7
Hjelmqvist <mathias.hjelmqvist@gmail.com>
2023-02-27 17:44:45 +0100

    Movement and rotation
    Moving with left joystick.
    Rotate with right joystick.
    The player collider follows when player moves IRL.
    Fixes looking through walls and stuff like that too.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] SteamVR_Action_Vector2 movementAction = SteamVR_Input.GetVector2Action("Movement");
+    [SerializeField] float movementSpeed = 3;
+
+    [Space(10)]
+    [SerializeField] SteamVR_Action_Vector2 rotationAction = SteamVR_Input.GetVector2Action("Rotation");
+    [SerializeField] float rotationSpeed = 180;
+
+    CharacterController controller;
+    Player player;
+
+    Vector3 gravity = Physics.gravity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        player = Player.instance;
+    }
+
+    void Update()
+    {
+        Move();
+        ApplyPhysics();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        Vector2 input = movementAction.axis;
+        Vector3 direction = player.hmdTransform.TransformDirection(new Vector3(input.x, 0, input.y));
+        Vector3 movement = direction * movementSpeed * Time.deltaTime;
+        controller.Move(movement);
+    }
+
+    private void ApplyPhysics()
+    {
+        controller.Move(gravity * Time.deltaTime);
+    }
+
+    private void Rotate()
+    {
+        Vector2 input = rotationAction.GetAxis(SteamVR_Input_Sources.RightHand);
+        Vector3 rotation = new Vector3(0, input.x) * rotationSpeed * Time.deltaTime;
+        transform.Rotate(rotation);
+    }
+}
